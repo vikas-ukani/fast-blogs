876f7cabb9c9ae3280ce7d4054d88d93145f9b69
Vikas Ukani <vikasukani5@gmail.com>
2021-08-29 16:56:55 +0530

    Final Code Deployment.

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-from fastapi import FastAPI
-from routers.route import router as DefaultRoutes
-from routers.users import router as UserRoutes
-from routers.posts import router as PostsRoutes
+import uvicorn
 
-# Start FastAPI Application...
-app = FastAPI()
-
-# Include Routes
-app.include_router(DefaultRoutes)
-app.include_router(UserRoutes)
-app.include_router(PostsRoutes)
-
-
-# Application Default Route
-@app.get('/', tags=['root'])
-async def index() -> dict:
-    return {"message": " Welcome to the Fast Blogs..."}
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-import uvicorn
 
-if __name__ == '__main__':
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+from fastapi import FastAPI
+
+from routers.route import router as DefaultRoutes
+from routers.users import router as UserRoutes
+from routers.posts import router as PostsRoutes
+
+# Start FastAPI Application...
+app = FastAPI()
+
+# Include Routes
+app.include_router(DefaultRoutes)
+app.include_router(UserRoutes)
+app.include_router(PostsRoutes)
+
+
+# Application Default Route
+@app.get('/', tags=['root'])
+async def index() -> dict:
+    return {"message": " Welcome to the FastAPI, Please visit `/docs` to access an APIs..."}
+
